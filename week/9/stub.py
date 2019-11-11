45fcc244628540611aedeb32cc0ff18eda3c0a44
ReznikovEgor <ReznikovEgorCS@gmail.com>
2019-11-10 20:51:10 -0500

    Finished HW9

--- week/9/stub.py
+++ week/9/stub.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python2
-
 import sys
 import struct
-
+import time
+import binascii
 
 # You can use this method to exit on failure conditions.
 def bork(msg):
@@ -25,6 +24,15 @@ with open(sys.argv[1], 'rb') as fpff:
 # Hint: you might find it easier to use an index/offset variable than
 # hardcoding ranges like 0:8
 magic, version = struct.unpack("<LL", data[0:8])
+timestamp = struct.unpack("<L", data[8:12])
+author = struct.unpack("<8s", data[12:20])
+section = struct.unpack("<L", data[20:24])
+
+section = section[0]
+author = author[0].decode()
+timedate = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(timestamp[0]))
+
+
 
 if magic != MAGIC:
     bork("Bad magic! Got %s, expected %s" % (hex(magic), hex(MAGIC)))
@@ -35,8 +43,58 @@ if version != VERSION:
 print("------- HEADER -------")
 print("MAGIC: %s" % hex(magic))
 print("VERSION: %d" % int(version))
+print("TIMESTAMP: %s" % timedate)
+print("AUTHOR: %s" % author)
+print("SECTION %d" % int(section))
+
 
 # We've parsed the magic and version out for you, but you're responsible for
 # the rest of the header and the actual FPFF body. Good luck!
 
 print("-------  BODY  -------")
+i = 1
+offset = 24
+while i <= section:
+    stype, slen = struct.unpack("<LL", data[offset:offset+8])
+    offset+=8
+    
+    #1, 6, 8, 1, 1
+
+    # SECTION_ASCII (0x1)
+    if stype == 1:
+        print("Section Type: ASCII")
+        print("Section Length %d" % slen)
+        text = struct.unpack("{}s".format(slen), data[offset:offset+slen])
+        print("TEXT: " + text[0].decode())
+    # SECTION_UTF8 (0x2) -- UTF-8-encoded text.
+    # if stype == 2:
+    # SECTION_WORDS (0x3) -- Array of words.
+    # if stype == 3:
+    # # SECTION_DWORDS (0x4) -- Array of dwords.
+    # if stype == 4:
+    # # SECTION_DOUBLES (0x5) -- Array of doubles.
+    # if stype == 5:
+    # # SECTION_COORD (0x6) -- (Latitude, longitude) tuple of doubles.
+    if stype == 6:
+        print("Section Type: COORD")
+        print("Section Length %d" % slen)
+        cords = struct.unpack("dd", data[offset:offset+slen])
+        print("COORDINATES: " + "{}".format(cords[0]) + " " + "{}".format(cords[1]))
+    # # SECTION_REFERENCE (0x7) -- The index of another section.
+    # if stype == 7:
+    # SECTION_PNG (0x8) -- Embedded PNG image.
+    if stype == 8:
+        print("Section Type: PNG")
+        print("Section Length %d" % slen)
+        png_head = binascii.unhexlify("89504e470d0a1a0a")
+        f = open('png.png', 'wb')
+        f.write(png_head)
+        f.write(data[offset:offset+slen])
+    # # SECTION_GIF87 (0x9) -- Embedded GIF87.
+    # if stype == 9:
+    # # SECTION_GIF89 (0xA) -- Embedded GIF89.
+    # if stype == 10:
+
+
+    offset+=slen
+    i+=1
--- week/9/writeup/README.md
+++ week/9/writeup/README.md
@@ -1,15 +1,60 @@
 # Writeup 9 - Forensics II
 
-Name: *PUT YOUR NAME HERE*
-Section: *PUT YOUR SECTION NUMBER HERE*
+Name: Egor Reznikov
+Section: 0101
 
 I pledge on my honor that I have not given or received any unauthorized assistance on this assignment or examination.
 
-Digital acknowledgement: *PUT YOUR NAME HERE*
+Digital acknowledgement: Egor Reznikov
 
 
 ## Assignment details
 
 ### Part 1 (45 Pts)
 
+1. Warmup: what IP address has been attacked?
+    142.93.136.81
+
+2. What kind of assessment tool(s) were the attackers using against the victim machine? List the name(s) of the tool(s) as well.
+    nmap was the tool that was used due to the hackers attempting to look at all the ports to find a vulnerable port.
+
+3. What are the hackers' IP addresses, and where are they connecting from?
+    159.203.113.181, Clifton, New Jersey, United States, North America
+
+4. What port are they using to steal files on the server?
+    55914, 21
+
+5. Which file did they steal? What kind of file is it? Do you recognize the file?
+    find_me.jpeg
+
+6. Which file did the attackers leave behind on the server?
+    greetz.fpff
+
+7. What is a countermeasure to prevent this kind of intrusion from happening again? Note: disabling the vulnerable service is not an option.
+    One countermeasure to prevent this attack would be blacklisting an ip if you see that they are doing a port scan. One example we learned in class was that if you attempt to facebook.com, Facebook will blacklist your ip so that no one on your network can access the website. In the capture file you can see a bunch of different ports being scanned and you can see the source ip.
 ### Part 2 (55 Pts)
+1. When was greetz.fpff generated?
+    2019-03-27 00:15:05
+
+2. Who authored greetz.fpff?
+    fl1nch
+
+3. List each section, givnig us the data in it and it's type.
+    Section Type: ASCII
+    Section Length 24
+    TEXT: Hey you, keep looking :)
+    Section Type: COORD
+    Section Length 16
+    COORDINATES: 52.336035 4.880673
+    Section Type: PNG
+    Section Length 202776
+    Section Type: ASCII
+    Section Length 44
+    TEXT: }R983CSMC_perg_tndid_u0y_yllufep0h{-R983CSMC
+    Section Type: ASCII
+    Section Length 80
+    TEXT: Q01TQzM4OVIte2hleV9oM3lfeTBVX3lvdV9JX2RvbnRfbGlrZV95b3VyX2Jhc2U2NF9lbmNvZGluZ30=
+
+4. Report at least one flag hidden in greetz.fpff. Any other flag found will count as bonus points towards the competition portion of the syllabus.
+    CMSC389R-{h0pefully_y0u_didnt_grep_CMSC389R}
+    CMSC389R-{w31c0me_b@ck_fr0m_spr1ng_br3ak}
